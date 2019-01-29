61fa43725fb16638b0eac44d4b67045c7b548364
Solofo <solofo.ralitera@gmail.com>
2019-01-29 22:37:06 +0300

    Tacho

--- fam/fam.ino
+++ fam/fam.ino
@@ -3,7 +3,10 @@
 // Install Joystick library from https://github.com/MHeironimus/ArduinoJoystickLibrary
 
 // PWM 12V
-// https://arduino103.blogspot.com/2012/07/commande-moteur-avec-transitor-mosfet.html
+// help: https://forum.arduino.cc/index.php?topic=92776.0 => avec circuit simple pour pwm 12v
+// https://forum.allaboutcircuits.com/threads/tach-frequency-calculation.98285/ => avec correspondance rpm/hz
+// https://www.youtube.com/watch?v=n7bltLKnRrs / https://github.com/daniel-centore/arduino-tone-library => ne fonctionne pas sur 1.8.7 leonardo
+// Build-in function: https://www.arduino.cc/reference/en/language/functions/advanced-io/tone/
 
 #include <Joystick.h>
 
@@ -16,11 +19,9 @@ Joystick_ Joystick;
 int Fam = 0;         
 int tachoPin = 5;
 
-//const bool initAutoSendState = true; 
-
 void setup()
 {
-  Serial.begin(9600);
+  // Serial.begin(9600);
   
   Joystick.begin();
   Joystick.setXAxis(0);
@@ -41,13 +42,13 @@ void setup()
 }
    
 void loop(){
-  
-  Serial.println(analogRead(A1));                
-  
+
+  /**** Fam ****/
   Fam = map(analogRead(A0), 624, 480, 1023, 0);         
   Joystick.setBrake(Fam);
 
-  analogWrite(tachoPin, analogRead(A1) / 4);
+  // Pour 4 cylindres => Hz = RPM / 30
+  tone(tachoPin, 500 / 30);
   
   delay (50);
 }
