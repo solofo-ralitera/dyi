0d614a57c452f3823d3f4b4253fbadcfe6833ad5
Solofo <solofo.ralitera@gmail.com>
2024-08-24 21:22:23 +0300

    test analog

--- a10/modules/test-analog-button/test-analog-button.ino
+++ a10/modules/test-analog-button/test-analog-button.ino
@@ -1,10 +1,33 @@
+#define ANALOG_PIN A2
+#define ANALOG_SAMPLE 15
+
+int numSample = 0;
+int samples[ANALOG_SAMPLE];
+int analogMean = 0;
+int analogValue = 0;
+
 void setup() {
   Serial.begin(9600);
 
-  pinMode(A6, INPUT_PULLUP);
+  pinMode(ANALOG_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  Serial.println(analogRead(A6));
+  analogValue = analogRead(ANALOG_PIN);
 
+  if (numSample < ANALOG_SAMPLE) {
+    samples[numSample] = analogValue;
+    numSample++;
+  } else if (numSample == ANALOG_SAMPLE) {
+    // Calc moyenne
+    long sum = 0L;
+    for (int i = 0; i < ANALOG_SAMPLE; i++) {
+      sum += samples[i];
+      samples[i] = 0; // Reset each value
+    }
+    analogMean = sum / ANALOG_SAMPLE;
+    numSample = 0;
+
+    Serial.println(analogMean);
+  }
 }
